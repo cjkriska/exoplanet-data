745236ecab8337ef4643deaf4aea1d7b8d8aa032
cjkriska <charleskriska@gmail.com>
2022-08-17 19:29:26 -0500

    add test api endpoint and api documentation

--- src/main/java/com/charliekriska/exoplanetdata/controller/ExoplanetController.java
+++ src/main/java/com/charliekriska/exoplanetdata/controller/ExoplanetController.java
@@ -8,7 +8,7 @@ public class ExoplanetController {
 
     @GetMapping("/test")
     public String getTest() {
-        return "Test";
+        return "https://exoplanetarchive.ipac.caltech.edu/TAP/sync?query=select+pl_name,hostname,pl_letter,discoverymethod,disc_year,pl_orbper,pl_orbsmax,pl_rade,pl_bmasse,pl_dens,pl_orbeccen,pl_eqt,sy_dist+from+pscomppars+order+by+sy_dist+asc&format=json";
     }
 
 }
