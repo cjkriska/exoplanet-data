a2697d6a875593d48918cee2db938d26f34009ef
cjkriska <charleskriska@gmail.com>
2022-08-22 16:36:47 -0500

    minor changes to db testing file

--- src/test/java/com/charliekriska/exoplanetdata/dao/ExoPlanetDbImplTest.java
+++ src/test/java/com/charliekriska/exoplanetdata/dao/ExoPlanetDbImplTest.java
@@ -1,32 +1,22 @@
 package com.charliekriska.exoplanetdata.dao;
 
+import com.charliekriska.exoplanetdata.config.Config;
 import okhttp3.mockwebserver.MockWebServer;
-import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.reactive.function.client.WebClient;
 
-import java.io.IOException;
 
 public class ExoPlanetDbImplTest {
 
     private MockWebServer mockServer;
-
-    @Autowired
     private ExoPlanetDao exoPlanetDao;
 
-    @BeforeAll
-    void setUp() throws IOException {
-        mockServer = new MockWebServer();
-        mockServer.start();
-    }
-    @AfterAll
-    void tearDown() throws IOException {
-        mockServer.shutdown();
-    }
     @BeforeEach
     void initialize() {
-        String baseUrl = String.format("http://localhost:%s", mockServer.getPort());
+        mockServer = new MockWebServer();
+        Config config = new Config();
+        config.setBaseUrl(mockServer.url("/").url().toString());
+        exoPlanetDao = new ExoPlanetDaoImpl(WebClient.builder(), config);
     }
 
 }
