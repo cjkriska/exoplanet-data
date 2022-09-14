871747f8e5dfb57e0449ff42b050b499dd89100d
cjkriska <charleskriska@gmail.com>
2022-09-14 17:02:21 -0500

    remove dupl cors

--- src/main/java/com/charliekriska/exoplanetdata/controller/ExoplanetController.java
+++ src/main/java/com/charliekriska/exoplanetdata/controller/ExoplanetController.java
@@ -11,7 +11,6 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
-@CrossOrigin(origins = "http://localhost:3000")
 @RestController
 @CrossOrigin
 @RequestMapping("v1")
