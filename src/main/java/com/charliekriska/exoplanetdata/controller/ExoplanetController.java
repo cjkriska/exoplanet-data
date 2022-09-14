fe07d7ffe4398103255b7b5259424b3fd5e5c72d
cjkriska <charleskriska@gmail.com>
2022-09-14 16:51:38 -0500

    add cors

--- src/main/java/com/charliekriska/exoplanetdata/controller/ExoplanetController.java
+++ src/main/java/com/charliekriska/exoplanetdata/controller/ExoplanetController.java
@@ -4,6 +4,7 @@ import com.charliekriska.exoplanetdata.model.Planet;
 import com.charliekriska.exoplanetdata.model.Star;
 import com.charliekriska.exoplanetdata.model.StarSystem;
 import com.charliekriska.exoplanetdata.service.ExoPlanetService;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
+@CrossOrigin
 @RequestMapping("v1")
 public class ExoplanetController {
 
